9f074c343b329fe821d78a815b6c253d6e68ffac
DEBRAJ MONDAL <debraj.abv@gmail.com>
2024-05-07 21:38:31 +0530

    files added

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/debu',(req,res)=>{
+    res.send("This is debus page")
+})
+
+app.get('/debu/new',(req,res)=>{
+    res.send("This is debus new page")
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
+
+git 
